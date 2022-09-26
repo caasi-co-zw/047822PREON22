1a4762b846ffc915bcd3c904bba04c5d3316e248
isaac-machakata <isaac@caasi.co.zw>
2022-09-26 10:30:06 +0200

    Restructured

---
+++ Parking Systemm/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim period(14) As Integer
+        Dim num As Integer
+        Console.WriteLine(num)
+
+    End Sub
+
+End Module
---
+++ Task1.vb
@@ -0,0 +1,47 @@
+Sub Main()
+
+    ''' DB STRUCTURE
+    ''' [ (An array of all the days (14))
+    '''     [ (Each day is an array of Spaces available (20))
+    '''         { (Each parking lot is a dictionary containing all the details needed)
+    '''             "license": 123232,
+    '''             "name": "Shaun K"
+    '''         }
+    '''     ]
+    ''' ]
+    '''
+    Dim Bookings As New List(Of Dictionary(Of Integer,Integer,String))()
+    Dim Days(14) As Integer
+    Dim Spaces(20) As Integer
+
+    Function isDayAvailable(Day As Int)
+        For Each Booking In Bookings
+            If Booking = Day Then
+                If Not IsNothing i Then
+                    Return True
+                    Exit For
+                Else
+                    Return False
+                    Exit For
+                End If
+            End If
+        Next
+    End Function
+
+    Function getName(Day As Int,Space As Int)
+        Days(Day)
+    End Function
+
+    Function getDay()
+
+    End Function
+
+    Function AddBooking(Day As Integer,Space As Integer,License As String)
+        Bookings.Add(
+            New Dictionary(Of Integer, Integer,String)() From {
+                {"Day",Day}
+                }
+            )
+    End Function
+
+End Sub
