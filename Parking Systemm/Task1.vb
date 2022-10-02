0fa7f8ceaeaeff24264dcc39309fbece20f9ddc4
isaac-machakata <isaac@caasi.co.zw>
2022-10-02 18:56:04 +0200

    Should be working :|

--- Parking Systemm/Task1.vb
+++ Parking Systemm/Task1.vb
@@ -1,17 +1,26 @@
+'   ATTENTION-------------------------------------------------------------------|
+'   My assumptions are that it will work ;) BUT                                 |
+'   Please test this task thoroughly. You may want look out for the following   |
+'   ----------------------------------------------------------------------------|
+'   TEST       = Test the feature or function                                   |
+'   BONUS TIP  = Incase you find yourself in a different situation              |
+'   ----------------------------------------------------------------------------|
+
 Imports System.Collections.Generic
+
 Module Task1
 
     'Global Variables
     Public MAX_DAYS As Integer = 14
     Public MAX_SLOTS As Integer = 20
-
-    'Licenses and Lots are indexed by day
-    Public Names(MAX_DAYS) As List(Of String)()
-    Public Licenses(MAX_DAYS) As List(Of String)()
-    Public Lots(MAX_DAYS) As List(Of String)()
+    Public Names(MAX_DAYS) As List(Of String) = New List(Of String)
+    Public Licenses(MAX_DAYS) As List(Of String) = New List(Of String)
+    Public Lots(MAX_DAYS) As List(Of String) = New List(Of String)
 
     Sub Main()
         Dim MenuOption As Integer = 1
+
+        'While the user's option is not 3, show menu
         While MenuOption < 3
 
             'Make sure value is in range
@@ -49,7 +58,9 @@ Module Task1
         Dim License As String
         Dim AddAnother As String = "Y"
 
+        'While the user has entered y/Y continue to add more data
         While AddAnother.ToUpper = "Y"
+
             'Record reservation day
             Console.WriteLine("Day to reserve: ")
             Day = Console.ReadLine()
@@ -59,6 +70,13 @@ Module Task1
                 Continue While
             End If
 
+            'BONUS TIP
+            '   If you have to use real dates & use a real database,
+            '   you can convert the date to a timestamp and add all the details to the day
+            '   timestamped.
+            '   To check if there's any available spaces,
+            '   Simply query to check if you have less than 15 results (ie. Results < 15)
+            '
             'Check for availableslots for the day
             If Lots(Day-1) isNot Nothing  And Lots(Day-1).Count >= MAX_SLOTS Then
                 Console.WriteLine("Day {0} is fully booked. Try another day.",Day)
@@ -83,6 +101,16 @@ Module Task1
 
     ' Clears all records
     Sub ClearReservations()
+
+        'TEST
+        '   See if data is cleared for all days,
+        '   because you may have to loop through each day, clearing records
+        '   or simply redim the three variables like ReDim Names(MAX_DAYS) As List(Of String) = New List(Of String)
+        '   if it works.
+        '   Also if data is not to be cleared on prompt - simply call this function on line 25 - in the loop
+        '   but only if you have also switched to the timestamp version of the database or you'll have to call it on
+        '   line 22 ;)
+
         Names(MAX_DAYS).Clear()
         Licenses(MAX_DAYS).Clear()
         Lots(MAX_DAYS).Clear()
